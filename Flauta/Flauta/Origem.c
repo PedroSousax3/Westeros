a140423e342099ee4cefcc894a66702b1154f644
PedroSousax3 <pedro.sousax3@gmail.com>
2022-11-26 23:13:54 -0300

    Adicionando arquivo Origem.h

--- Flauta/Flauta/Origem.c
+++ Flauta/Flauta/Origem.c
@@ -1,28 +1,4 @@
-﻿//Libs externas
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro5.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_native_dialog.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_font.h>
-#include <stdbool.h>
-
-//Importações
-//#include "Cabecalho/Pagina.h"
-#include "Cabecalho/Utils/Utilitario.h"
-#include "Cabecalho/Posicao.h"
-#include "Cabecalho/Movimento.h"
-#include "Cabecalho/Personagem.h"
-#include "Cabecalho/Pagina.h"
-#include "PaginaCombinacao.h"
-#include "Cabecalho/Cenario.h"
-#include "Cabecalho/Utils/Imagem.h"
-#include "Cabecalho/Missao.h"
-#include <locale.h>
+﻿#include "Origem.h"
 
 const int fps = 60;
 int fpsAnimacao = 0;
